f07321227dfbfe280bf4cb16f3a6581041b8650a
Julio Cesar <jccintr@gmail.com>
2025-07-25 20:45:19 -0300

    implementado RadioServiceTests

--- src/test/java/com/jcsoftware/radios/services/RadioServiceTests.java
+++ src/test/java/com/jcsoftware/radios/services/RadioServiceTests.java
@@ -2,12 +2,17 @@ package com.jcsoftware.radios.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,10 +30,13 @@ import com.jcsoftware.radios.entities.Radio;
 import com.jcsoftware.radios.entities.dtos.CategoryDTO;
 import com.jcsoftware.radios.entities.dtos.NewRadioDTO;
 import com.jcsoftware.radios.entities.dtos.RadioDTO;
+import com.jcsoftware.radios.entities.dtos.UpdateRadioDTO;
 import com.jcsoftware.radios.entities.enums.State;
 import com.jcsoftware.radios.repositories.CategoryRepository;
 import com.jcsoftware.radios.repositories.CityRepository;
 import com.jcsoftware.radios.repositories.RadioRepository;
+import com.jcsoftware.radios.services.exceptions.DatabaseIntegrityViolationException;
+import com.jcsoftware.radios.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
 public class RadioServiceTests {
@@ -52,9 +60,8 @@ public class RadioServiceTests {
         Long categoryId = 10L;
 
         City mockCity = new City(cityId, "São Paulo", State.SP);
-        Category mockCategory = new Category();
-        mockCategory.setId(categoryId);
-        mockCategory.setName("Gospel");
+        Category mockCategory = new Category(categoryId,"Gospel");
+      
 
         when(cityRepository.getReferenceById(cityId)).thenReturn(mockCity);
         when(categoryRepository.getReferenceById(categoryId)).thenReturn(mockCategory);
@@ -120,7 +127,7 @@ public class RadioServiceTests {
 	
 	 @Test
 	 void findAllPaged_ShouldReturnPageOfRadioDTO() {
-		 // Arrange
+		 
 		 Long cityId = 1L;
 	     Long categoryId = 10L;
 	     Long radioId = 20L;
@@ -179,10 +186,10 @@ public class RadioServiceTests {
 			         "http://images.teste.com/bandvale.jpg",
 			         false,
 			         mockCity2);
-			  mockRadio1.getCategories().add(mockCategory1);
+		  mockRadio2.getCategories().add(mockCategory2);
 			  
 		  Sort sort = Sort.by("name");
-		  List<Radio> radioList = List.of(mockRadio2, mockRadio2);
+		  List<Radio> radioList = List.of(mockRadio1, mockRadio2);
 		  when(repository.findAll(sort)).thenReturn(radioList);
 		  
 		  List<RadioDTO> result = service.findAll();
@@ -196,6 +203,161 @@ public class RadioServiceTests {
 	      assertEquals(radioList.get(1).getCategories().size(), result.get(1).categories().size());
 	      verify(repository).findAll(sort);
 		 
+	 }
+	 
+	 @Test
+	 void findById_ShouldReturnRadioDTO_WhenRadioExist() {
+		 
+		 Long existingRadioId = 1L;
+		 City mockCity = new City(1L, "Rio de Janeiro", State.RJ);
+		 Category mockCategory = new Category(1L,"Gospel");
+		 Radio mockRadio = new Radio(
+				 1L,
+				 "Rádio 93 FM",
+		         "93 FM",
+		         "http://stream.teste.com/stream93fm",
+		         "http://images.teste.com/93fm.jpg",
+		         false,
+		         mockCity);
+		  mockRadio.getCategories().add(mockCategory);
+		  
+		  when(repository.findById(existingRadioId)).thenReturn(Optional.of(mockRadio));
+		  
+		  RadioDTO result = service.findById(existingRadioId);
+		  
+		  assertNotNull(result);
+	      assertEquals(existingRadioId, result.id());
+	      assertEquals(mockRadio.getName(), result.name());
+	      assertEquals(mockRadio.getCity().getName(),result.city().name());
+	      assertEquals(mockRadio.getCategories().size(),result.categories().size());
+          verify(repository).findById(existingRadioId);
+	 }
+	 
+	 
+	 @Test
+	 void findById_ShouldThrowResourceNotFoundException_WhenRadioDoesNotExist() {
+		 
+		 Long nonExistentId = 99L;
+		 
+		 when(repository.findById(nonExistentId)).thenReturn(Optional.empty());
+		 
+		 assertThrows(ResourceNotFoundException.class, () -> {
+	            service.findById(nonExistentId);
+	     });
+
+	     verify(repository).findById(nonExistentId);
+	 }
+	 
+	 @Test
+	 void delete_ShouldSucceed_WhenRadioExistsAndIsNotReferenced() {
+		 
+		 Long validId = 99L;
+		 
+		 when(repository.existsById(validId)).thenReturn(true);
+		 
+		 Assertions.assertDoesNotThrow(()->service.delete(validId));
+		 
+		 verify(repository).deleteById(validId);
+		 
+	 }
+	 
+	 
+	 @Test
+	 void delete_ShouldThrow_DatabaseIntegrityViolationException_WhenRadioIsReferenced() {
+		 Long referencedId = 99L;
+         when(repository.existsById(referencedId)).thenReturn(true);
+         doThrow(DatabaseIntegrityViolationException.class).when(repository).deleteById(referencedId);
+	     assertThrows(DatabaseIntegrityViolationException.class, () -> service.delete(referencedId));
+	     verify(repository, never()).delete(any());
+	 }
+	 
+	 @Test
+	 void delete_ShouldThrowResourceNotFoundException_WhenRadioDoesNotExist() {
+		 
+		 Long nonExistentId = 99L;
+		 
+		 when(repository.existsById(nonExistentId)).thenReturn(false);
+		 
+		 assertThrows(ResourceNotFoundException.class, () -> service.delete(nonExistentId));
+	     verify(repository, never()).delete(any());
+	 }
+	 
+	 @Test
+	 void update_ShouldSucceed_WhenRadioExist() {
+		   Long categoryId = 1L;
+		   Long categoryId2 = 2L;
+		   Long existingRadioId = 1L;
+		   City mockCity = new City(1L, "Rio de Janeiro", State.RJ);
+	       Category mockCategory = new Category(categoryId,"Gospel");
+	       Category mockCategory2 = new Category(categoryId2,"Notícias");
+	       CategoryDTO mockCategoryDTO = new CategoryDTO(mockCategory);
+	       CategoryDTO mockCategoryDTO2 = new CategoryDTO(mockCategory2);
+	       
+	       Radio mockRadio = new Radio(
+	    		   existingRadioId,
+				 "Rádio 93 FM",
+		         "93 FM",
+		         "http://stream.teste.com/stream93fm",
+		         "http://images.teste.com/93fm.jpg",
+		         false,
+		         mockCity);
+		   mockRadio.getCategories().add(mockCategory);
+		   
+		   UpdateRadioDTO updateDto = new UpdateRadioDTO(
+				   "Rádio 93 FM Updated",
+				   "93 FM",
+				   "http://stream.teste.com/stream93fm",
+			       "http://images.teste.com/93fm.jpg",
+			       false,
+			       1L,
+			       List.of(mockCategoryDTO,mockCategoryDTO2)
+				   );
+		   
+		   Radio updatedRadio = new Radio();
+		   updatedRadio.setId(existingRadioId);
+		   updatedRadio.setName(updateDto.name());
+		   updatedRadio.setShortName(updateDto.shortName());
+		   updatedRadio.setStreamUrl(updateDto.streamUrl());
+		   updatedRadio.setImageUrl(updateDto.imageUrl());
+		   updatedRadio.setHsl(updateDto.hsl());
+		   updatedRadio.setCity(mockCity);
+		   updatedRadio.getCategories().add(mockCategory);
+		   updatedRadio.getCategories().add(mockCategory2);
+		   
+		   when(repository.getReferenceById(existingRadioId)).thenReturn(mockRadio);
+		   when(categoryRepository.getReferenceById(categoryId)).thenReturn(mockCategory);
+		   when(categoryRepository.getReferenceById(categoryId2)).thenReturn(mockCategory2);
+		   when(repository.save(any(Radio.class))).thenReturn(updatedRadio);
+		   
+		   RadioDTO result = service.update(existingRadioId, updateDto);
+		   
+		   assertNotNull(result);
+		   assertEquals(existingRadioId, result.id());
+		   assertEquals(updateDto.name(), result.name());
+		   assertEquals(updateDto.categories().size(),result.categories().size());
+	       verify(repository).getReferenceById(existingRadioId);
+	 }
+	 
+	 @Test
+	 void update_ShouldThrowResourceNotFoundException_WhenRadioDoesNotExist() {
+		 
+		 Long nonExistentId = 99L;
+		 Category mockCategory = new Category(1L,"Gospel");
+		 CategoryDTO mockCategoryDTO = new CategoryDTO(mockCategory);
+		 UpdateRadioDTO updateDto = new UpdateRadioDTO(
+				   "Rádio 93 FM Updated",
+				   "93 FM",
+				   "http://stream.teste.com/stream93fm",
+			       "http://images.teste.com/93fm.jpg",
+			       false,
+			       1L,
+			       List.of(mockCategoryDTO)
+				   );
+
+		 when(repository.getReferenceById(nonExistentId)).thenThrow(ResourceNotFoundException.class);
+       
+		 assertThrows(ResourceNotFoundException.class, () -> service.update(nonExistentId, updateDto));
+	     verify(repository, never()).save(any());
 	 }
 	 
 
