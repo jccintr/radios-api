de522bd58f85c3c3522f6a333f27c268391c9a23
Julio Cesar <jccintr@gmail.com>
2025-07-26 02:26:18 -0300

    rafatorado CategoryServiceTests

--- src/test/java/com/jcsoftware/radios/services/CategoryServiceTests.java
+++ src/test/java/com/jcsoftware/radios/services/CategoryServiceTests.java
@@ -2,7 +2,10 @@ package com.jcsoftware.radios.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -18,6 +21,7 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -27,19 +31,24 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.jcsoftware.radios.entities.Category;
+import com.jcsoftware.radios.entities.City;
+import com.jcsoftware.radios.entities.Radio;
 import com.jcsoftware.radios.entities.dtos.CategoryDTO;
 import com.jcsoftware.radios.entities.dtos.NewCategoryDTO;
+import com.jcsoftware.radios.entities.dtos.RadioDTO;
+import com.jcsoftware.radios.entities.enums.State;
 import com.jcsoftware.radios.repositories.CategoryRepository;
 import com.jcsoftware.radios.services.exceptions.DatabaseIntegrityViolationException;
 import com.jcsoftware.radios.services.exceptions.ResourceNotFoundException;
 import com.jcsoftware.radios.tests.Factory;
 
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class CategoryServiceTests {
 	
 	@InjectMocks
 	private CategoryService service;
+	/*
 	private Category category;
 	private CategoryDTO categoryDTO;
 	private List<Category> categoryList;
@@ -48,12 +57,13 @@ public class CategoryServiceTests {
 	private Long nonExistingId;
 	private Long dependentId;
 	private Pageable pageable;
-	
+	*/
 	@Mock
 	private CategoryRepository repository;
 	
 	@BeforeEach
 	void setup() throws Exception {
+		/*
 		existingId = 1L;
 		nonExistingId = 1000L;
 		dependentId = 10L;
@@ -77,90 +87,167 @@ public class CategoryServiceTests {
 		Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
 		Mockito.when(repository.existsById(dependentId)).thenReturn(true);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+		*/
 	}
 	
 	@Test
-	public void insert_ShouldReturnCategoryDTO() {
+	public void insert_ShouldPersistCategoryAndReturnCategoryDTO() {
+		
 		NewCategoryDTO newCategoryDTO = new NewCategoryDTO("Esportes");
+		
+		Category savedCategory = new Category(1L,newCategoryDTO.name());
+		
+		
+		when(repository.save(any(Category.class))).thenReturn(savedCategory);
+		
 		CategoryDTO result = service.insert(newCategoryDTO);
+		
 		assertEquals(newCategoryDTO.name(), result.name());
         verify(repository, times(1)).save(any(Category.class));
 		
 	}
 	
 	@Test
-	public void findById_ShouldReturnCategoryDTOWhenIdExists() {
+	public void findById_ShouldReturnCategoryDTO_WhenIdExists() {
+		
+		Long existingId = 1L;
+		Category mockCategory = new Category(1L,"Gospel");
+		
+		when(repository.findById(existingId)).thenReturn(Optional.of(mockCategory));
+		
 	    CategoryDTO result = service.findById(existingId);
+	    
 		Assertions.assertNotNull(result);
+		 assertEquals(mockCategory.getName(), result.name());
+		 
 	}
 	
+	
 	@Test
-	public void findById_ShouldThrowResourceNotFoundExceptionOWhenIdDoesNotExists() {
+	public void findById_ShouldThrowResourceNotFoundExceptionO_WhenCategoryDoesNotExists() {
+		
+		 Long nonExistentId = 99L;
+		 
+		 when(repository.findById(nonExistentId)).thenReturn(Optional.empty());
+		 
 		Assertions.assertThrows(ResourceNotFoundException.class,()->{
-			service.findById(nonExistingId);
+			service.findById(nonExistentId);
 		});
+		verify(repository).findById(nonExistentId);
+		
+	}
+	
+	
+	@Test
+	public void delete_ShouldSucceed_WhenCategoryExistsAndIsNotReferenced() {
+		
+		Long validId = 5L;
+		
+        when(repository.existsById(validId)).thenReturn(true);
+		 
+		 Assertions.assertDoesNotThrow(()->service.delete(validId));
+		 
+		 verify(repository).deleteById(validId);
+	}
+	
+	 @Test
+	 void delete_ShouldThrowResourceNotFoundException_WhenCategoryDoesNotExist() {
+		
+         Long nonExistentId = 99L;
+		 
+		 when(repository.existsById(nonExistentId)).thenReturn(false);
+		 
+		 assertThrows(ResourceNotFoundException.class, () -> service.delete(nonExistentId));
+	     verify(repository, never()).delete(any());
+	     
+	}
+	
+	 @Test
+	 void delete_ShouldThrow_DatabaseIntegrityViolationException_WhenCategoryIsReferenced() {
+		 
+		 Long referencedId = 99L;
+		 
+         when(repository.existsById(referencedId)).thenReturn(true);
+         
+         doThrow(DatabaseIntegrityViolationException.class).when(repository).deleteById(referencedId);
+         
+	     assertThrows(DatabaseIntegrityViolationException.class, () -> service.delete(referencedId));
+	     verify(repository, never()).delete(any());
+	     
+	 }
+	
+	
+	 @Test
+	 void update_ShouldSucceed_WhenCategoryExist() {
+		 
+		 Long validId = 5L;
+		 Category mockCategory = new Category(validId,"Gospel");
+		 CategoryDTO categoryDTO = new CategoryDTO(mockCategory);
+		 Category updatedCategory = new Category(validId,"Gospel Updated");
+		 
+		 when(repository.getReferenceById(validId)).thenReturn(mockCategory);
+		 when(repository.save(any(Category.class))).thenReturn(updatedCategory);
+		 
+		 CategoryDTO result = service.update(validId, categoryDTO);
+		 
+		 Assertions.assertNotNull(result);
+		 assertEquals(validId, result.id());
+		 assertEquals(updatedCategory.getName(), result.name());
+	     verify(repository).getReferenceById(validId);
 		
 	}
 	
 	@Test
-	public void deleteShouldDoNothingWhenIdExists() {
+	 void update_ShouldThrowResourceNotFoundException_WhenCategoryDoesNotExist() {
 		
-		Assertions.assertDoesNotThrow(()->{
-			service.delete(existingId);
-		});
+		 Long nonExistentId = 99L;
+		 Category mockCategory = new Category(nonExistentId,"Gospel");
+		 CategoryDTO categoryDTO = new CategoryDTO(mockCategory);
+		 
+		 when(repository.getReferenceById(nonExistentId)).thenThrow(ResourceNotFoundException.class);
+	       
+		 assertThrows(ResourceNotFoundException.class, () -> service.update(nonExistentId, categoryDTO));
+	     verify(repository, never()).save(any());
+	     
+	}
+	
+	
+	@Test
+    void findAllPaged_ShouldReturnSortedPagedOfCategoryDTOs() {
 		
-		Mockito.verify(repository,Mockito.times(1)).deleteById(existingId);
-	}
-	
-	@Test
-	public void delete_ShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
-		Assertions.assertThrows(ResourceNotFoundException.class,()->{
-			service.delete(nonExistingId);
-		});
-	}
-	
-	@Test
-	public void delete_ShouldThrowIntegrityViolationExceptionWhenDependentid() {
-		Assertions.assertThrows(DatabaseIntegrityViolationException.class,()->{
-			service.delete(dependentId);
-		});
-	}
-	
-	@Test
-	public void update_ShouldReturnCategoryDTOWhenIdExists() {
-		CategoryDTO result = service.update(existingId, categoryDTO);
-		Assertions.assertNotNull(result);
-	}
-	
-	@Test
-	public void update_ShouldThrowResourceNotFoundExceptionOWhenIdDoesNotExists() {
-		Assertions.assertThrows(ResourceNotFoundException.class,()->{
-			service.update(nonExistingId, categoryDTO);
-		});
-	}
-	
-	@Test
-    void findAllPaged_ShouldReturnPagedCategoryDTOs() {
+		Category mockCategory = new Category(1L,"Gospel");
+		
+		 List<Category> content = List.of(mockCategory);
+	     Pageable inputPageable = PageRequest.of(0, 5);
+	     Page<Category> page = new PageImpl<>(content);
+	     Pageable expectedPageable = PageRequest.of(0, 10, Sort.by("name").ascending());
+	     
+	     when(repository.findAll(expectedPageable)).thenReturn(page);
        
-        Page<CategoryDTO> result = service.findAllPaged(pageable);
-       
-        assertNotNull(result, "A página retornada não deve ser nula");
-        assertEquals(1, result.getTotalElements(), "O total de elementos deve ser 1");
-        assertEquals(1, result.getContent().size(), "A página deve conter 1 elemento");
-        assertEquals("Esportes", result.getContent().get(0).name(), "O nome da categoria deve ser 'Esportes'");
+        Page<CategoryDTO> result = service.findAllPaged(inputPageable);
+        
+        assertEquals(1, result.getTotalElements());
+	    assertEquals(mockCategory.getName(), result.getContent().get(0).name());
+	    verify(repository).findAll(expectedPageable);
         
     }
 	
 	@Test
-    void findAll_ShouldReturnSortedCategoryDTOs() {
+    void findAll_ShouldReturnSortedListOfCategoryDTOs() {
+		
+		 Category mockCategory1 = new Category(1L,"Gospel");
+		 Category mockCategory2 = new Category(1L,"MPB");
         
-        List<CategoryDTO> result = service.findAll();
-        
-        assertNotNull(result, "A lista retornada não deve ser nula");
-        assertEquals(2, result.size(), "A lista deve conter 2 elementos");
-        assertEquals("Esportes", result.get(0).name(), "A primeira categoria deve ser 'Esportes'");
-        assertEquals("Rock", result.get(1).name(), "A segunda categoria deve ser 'Rock'");
-
+		 Sort sort = Sort.by("name");
+		  List<Category> categoryList = List.of(mockCategory1, mockCategory2);
+		  when(repository.findAll(sort)).thenReturn(categoryList);
+		  
+		  List<CategoryDTO> result = service.findAll();
+		  
+		  assertEquals(categoryList.size(), result.size());
+	      assertEquals(categoryList.get(0).getName(), result.get(0).name());
+	      assertEquals(categoryList.get(1).getName(), result.get(1).name());
+	      verify(repository).findAll(sort);
     }
 	
 	
